c80dfdd8099ea43eaa5805a784ce8f4ad447b710
Lakshya Soni <69206138+lakshya018@users.noreply.github.com>
2022-07-22 01:16:10 +0530

    Update NoteItem.jsx

--- src/component/NoteItem/NoteItem.jsx
+++ src/component/NoteItem/NoteItem.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import NoteContext from '../../context/notes/noteContext';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './NoteItem.css';
 
