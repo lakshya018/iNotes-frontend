23880a51d475474e057a7c403b7a860b30d06ff0
Lakshya Soni <69206138+lakshya018@users.noreply.github.com>
2022-08-07 04:47:45 +0530

    Update Home.jsx

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-// import NoteContext from '../../context/notes/noteContext';
-// import Notes from '../Notes/Notes';
 import { Link } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import hero from '../../assets/hero.jpg'
+
+
 const Home = (props) => {
-    const loginStatus = localStorage.getItem('loginStatus');
-    if (loginStatus === true) {
-        toast.success('Logged in Successfully');
-        localStorage.setItem('loginStatus', false);
-    }
+    
     return (
         <div className=''>
             <div className="container">
@@ -22,22 +18,13 @@ const Home = (props) => {
                         <h1 className="display-5 fw-bold lh-1 mb-3">iNotes Web Application For making Notes</h1>
                         <p className="lead text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed praesentium odit numquam a ea minus obcaecati distinctio voluptates, non aut ipsa laudantium itaque nihil dolorem quod ullam. Quo ducimus odit nisi quae, et necessitatibus itaque tenetur eos, rem voluptates esse.</p>
                         <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                            <Link role="button" className="btn btn-primary btn-lg px-4 me-md-2" to="/login">Get Started</Link>
+                            {!localStorage.getItem('loginStatus') ?  <Link role="button" className="btn btn-primary btn-lg px-4 me-md-2" to="/login">Get Started</Link> :
+                              <Link role="button" className="btn btn-primary btn-lg px-4 me-md-2" to="/notes">Add a Note</Link>}
                         </div>
                     </div>
                 </div>
             </div>
-            <ToastContainer
-                position="top-center"
-                autoClose={1500}
-                theme="colored"
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover />
+            <ToastContainer />
         </div>
 
 
